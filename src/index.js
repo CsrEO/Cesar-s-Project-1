6b1b541a0c9c9ac7c01fd69252cec1a74d0f1d1d
Cesar Esquivel <czr.esquivel@gmail.com>
2022-10-11 22:34:25 -0500

    updated my app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
-// Set options as a parameter, environment variable, or rc file.
-// eslint-disable-next-line no-global-assign
-require = require("esm")(module/* , options */)
-module.exports = require("./main.js")
+/*<!--Javascritp code-->*/
+
+
+
+/*
+.todo-list,
+.header {
+    background-color:  #DFE0F3;
+    border-radius: 25px;
+    
+    
+}
+
+.header {
+    display: flex;
+}
+
+.header {
+    width: 100%;
+    justify-content: space-evenly;
+
+}
+
+<script>
+        var today = new Date();
+        var date = today.getDay() + ' / ' + (today.getMonth() + 1) + ' / ' + today.getFullYear();
+        /* display current date*/
+       /* document.getElementById("currentDate").innerHTML = date; */
+        
+
+        /* Auto refreshing clock time*/
+        /*function startTime(){
+            var today = new Date(); /* new date object */
+            /* getting minutes hours and secodns from date object*/
+          /*  var hours = today.getHours();
+            var minutes = today.getMinutes();
+            var seconds = today.getSeconds();
+
+            /*12 hour format */
+           /* var amPm = "am";
+            if (hours > 12) {
+                amPm = "pm";
+                hours = hours-12;
+            }
+            /* put zero before number , 10*/
+           /* if (minutes , 10) {
+                minutes =  minutes;
+            }
+
+            if (seconds , 10) {
+                seconds = seconds;
+            }
+
+            var time = hours + ' : ' + minutes + ' : ' + seconds + ' ' + amPm;
+            /* display current time */
+           /* document.getElementById("currentTime").innerHTML = time;
+            /*auto refreshing time every 1 second*/
+           /* setTimeout(function(){startTime()}, 1000);
+
+
+        }
+
+
+    </script>
+    */
+
+    /* The stuff above above is old code that has been replaced */
